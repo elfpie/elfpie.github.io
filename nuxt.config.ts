90d1f0ba12f8626df19ce78bc5ec7752fe082d9b
Felipe Brito <santos_felipe@outlook.com>
2024-04-16 17:37:35 -0300

    add static open graph

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,67 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxthub/core',
-    '@nuxt/fonts',
-    '@nuxt/ui',
-    '@vueuse/nuxt',
-  ],
+  modules: ["@nuxthub/core", "@nuxt/fonts", "@nuxt/ui", "@vueuse/nuxt"],
+
+  app: {
+    head: {
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Gifs gifs gifs! Gifs? Gifs?",
+        },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: "Elfpie Gifs",
+        },
+        {
+          hid: "og:url",
+          property: "og:url",
+          content: "https://elfpie.github.io/",
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content: "Gifs gifs gifs! Gifs? Gifs?",
+        },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content: "https://elfpie.github.io/social-card.jpeg",
+        },
+
+        // twitter card
+        {
+          hid: "twitter:title",
+          name: "twitter:title",
+          content: "Elfpie Gifs",
+        },
+        {
+          hid: "twitter:url",
+          name: "twitter:url",
+          content: "https://elfpie.github.io/",
+        },
+        {
+          hid: "twitter:description",
+          name: "twitter:description",
+          content: "Gifs gifs gifs! Gifs? Gifs?",
+        },
+        {
+          hid: "twitter:image",
+          name: "twitter:image",
+          content: "https://elfpie.github.io/social-card.jpeg",
+        },
+      ],
+    },
+  },
 
   hub: {
     blob: true,
   },
 
   ui: {
-    icons: ['simple-icons'],
+    icons: ["simple-icons"],
   },
 
   experimental: {
@@ -21,5 +70,5 @@ export default defineNuxtConfig({
 
   devtools: { enabled: true },
 
-  ssr: false
-})
+  ssr: false,
+});
